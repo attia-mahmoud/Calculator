e70355cb225bf514f88232c148cb57d2a7be2b6a
attia-mahmoud <attimoovy@gmail.com>
2022-06-12 17:37:37 +0300

    Update theme.js

--- theme.js
+++ theme.js
@@ -9,43 +9,38 @@ const ops = document.querySelectorAll(".operators");
 
 const html = document.querySelector("html");
 
-let position = 1;
+let position = localStorage.getItem("prefers-color-scheme") || 1;
 
 Switch.addEventListener("click", function () {
   switch (position) {
     case 1: {
       indicator.classList.add("go-right-1");
       indicator.classList.remove("go-left");
+      html.classList.add("theme-2");
+      html.classList.remove("theme-1");
       position = 2;
       break;
     }
     case 2: {
       indicator.classList.add("go-right-2");
       indicator.classList.remove("go-right-1");
+      html.classList.add("theme-3");
+      html.classList.remove("theme-2");
       position = 3;
       break;
     }
     case 3: {
       indicator.classList.add("go-left");
       indicator.classList.remove("go-right-2");
+      html.classList.add("theme-1");
+      html.classList.remove("theme-3");
       position = 1;
       break;
     }
   }
-
-  switch (position) {
-    case 1: {
-      html.classList.add("theme-1");
-      html.classList.remove("theme-3");
-      break;
-    }
-    case 2:
-      html.classList.add("theme-2");
-      html.classList.remove("theme-1");
-      break;
-    case 3:
-      html.classList.add("theme-3");
-      html.classList.remove("theme-2");
-      break;
-  }
+  prefers_color_scheme();
 });
+
+function prefers_color_scheme() {
+  localStorage.setItem("prefers-color-scheme", position);
+}
